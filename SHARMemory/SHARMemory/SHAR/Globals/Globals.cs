64636d5c5fa3ba437b8db616b8ab01682b6c340e
Joshua Hampson <josh@zhbot.org>
2023-03-16 02:38:52 +0000

    Update Globals.cs
    * Added some missing comments

--- SHARMemory/SHARMemory/SHAR/Globals/Globals.cs
+++ SHARMemory/SHARMemory/SHAR/Globals/Globals.cs
@@ -6,7 +6,13 @@ namespace SHARMemory.SHAR
     {
         private readonly Memory Memory;
 
+        /// <summary>
+        /// A reference to SHAR's <see cref="Classes.GameplayManager"/> static global.
+        /// </summary>
         public GameplayManager GameplayManager => Memory.CreateClass<GameplayManager>(Memory.ReadUInt32(Memory.SelectAddress(0x6C8998, 0x6C8958, 0x6C8958, 0x6C8990)));
+        /// <summary>
+        /// A reference to SHAR's <see cref="Classes.FeTextBible"/> static global.
+        /// </summary>
         public FeTextBible TextBible => Memory.CreateClass<FeTextBible>(Memory.ReadUInt32(Memory.SelectAddress(0x6C8944, 0x6C8904, 0x6C8904, 0x6C893C)));
 
         /// <summary>
@@ -14,9 +20,21 @@ namespace SHARMemory.SHAR
         /// </summary>
         public byte LevelCount => Memory.ReadByte(Memory.SelectAddress(0x4798A8, 0x479748, 0x479618, 0x4793D8) + 3);
 
+        /// <summary>
+        /// A reference to the handler for SHAR's <c>CharacterTune</c> statics.
+        /// </summary>
         public CharacterTuneHandler CharacterTune { get; }
+        /// <summary>
+        /// A reference to the handler for SHAR's <c>Cheat</c> statics.
+        /// </summary>
         public CheatsHandler Cheats { get; }
+        /// <summary>
+        /// A reference to the handler for SHAR's <c>FeTextBible</c> statics.
+        /// </summary>
         public FeTextBibleHandler FeTextBible { get; }
+        /// <summary>
+        /// A reference to the handler for SHAR's <c>TrafficManager</c> statics.
+        /// </summary>
         public TrafficManagerHandler TrafficManager { get; }
 
         internal Globals(Memory memory)
