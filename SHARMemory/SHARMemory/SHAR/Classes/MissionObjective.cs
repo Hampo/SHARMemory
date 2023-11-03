1aaff2f7a647cc62c05c2cfe8576a43f8b82b384
Proddy <josh@zhbot.org>
2023-11-03 23:33:36 +0000

    Start of MissionObjective

--- SHARMemory/SHARMemory/SHAR/Classes/MissionObjective.cs
+++ SHARMemory/SHARMemory/SHAR/Classes/MissionObjective.cs
@@ -6,8 +6,59 @@ namespace SHARMemory.SHAR.Classes
     [ClassFactory.TypeInfoName(".?AVMissionObjective@@")]
     public class MissionObjective : Class
     {
+        public enum DirectionalArrowType
+        {
+            Intersection = 0x01,
+            NearestRoad = 0x10,
+            Both = 0x11,
+            Neither = 0x100,
+            NumTypes = 4
+        }
+
+        public enum ObjectiveTypes
+        {
+            Invalid,
+            Goto,
+            Delivery,
+            Follow,
+            Destroy,
+            Race,
+            LoseTail,
+            TalkTo,
+            Dialogue,
+            GetIn,
+            Dump,
+            FMV,
+            Interior,
+            Coin,
+            DestroyBoss,
+            LoadVehicle,
+            PickupItem,
+            Timer,
+            BuyCar,
+            BuySkin,
+            GoOutside,
+            NumObjectives
+        };
+
         public MissionObjective(Memory memory, uint address, CompleteObjectLocator completeObjectLocator) : base(memory, address, completeObjectLocator) { }
 
-        // TODO
+        public DirectionalArrowType ArrowType
+        {
+            get => (DirectionalArrowType)ReadInt32(4);
+            set => WriteInt32(4, (int)value);
+        }
+
+        public ObjectiveTypes ObjectiveType
+        {
+            get => (ObjectiveTypes)ReadInt32(8);
+            set => WriteInt32(8, (int)value);
+        }
+
+        public bool Finished
+        {
+            get => ReadBoolean(12);
+            set => WriteBoolean(12, value);
+        }
     }
 }
