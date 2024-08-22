24096135987a7e43c7ed21fc84175c99175b88b9
Proddy <josh@zhbot.org>
2024-08-22 15:03:25 +0100

    Added some new Global patches

--- SHARMemory/SHARMemory/SHAR/Globals/Globals.cs
+++ SHARMemory/SHARMemory/SHAR/Globals/Globals.cs
@@ -47,6 +47,67 @@ public sealed partial class Globals
         set => Memory.WriteBytes(FlippableCarsAddress, value ? FlippableCarsEnable : FlippableCarsDisable);
     }
 
+    public readonly uint DefaultNumTurbosAddress;
+    /// <summary>
+    /// A property representing the default <see cref="Vehicle.NumTurbos"/> set in the <c>Vehicle</c> constructor in <see cref="Memory"/>.
+    /// <para>Because ASM, this also affects the default <see cref="Vehicle.WasHitByVehicleType"/> value, however this value should be updated before its ever checked so probably fine.</para>
+    /// </summary>
+    public int DefaultNumTurbos
+    {
+        get => Memory.ReadInt32(DefaultNumTurbosAddress);
+        set => Memory.WriteInt32(DefaultNumTurbosAddress, value);
+    }
+
+    private static readonly byte[] TurboEnable = [0x90, 0x90];
+    private static readonly byte[] TurboDisable1 = [0x75, 0x07];
+    private static readonly byte[] TurboDisable2 = [0xEB, 0x41];
+    private readonly uint TurboAddress1;
+    private readonly uint TurboAddress2;
+    /// <summary>
+    /// A property representing if <c>Turbo</c> is enabled in <see cref="Memory"/>.
+    /// </summary>
+    public bool TurboEnabled
+    {
+        get => Enumerable.SequenceEqual(Memory.ReadBytes(TurboAddress1, 2), TurboEnable) && Enumerable.SequenceEqual(Memory.ReadBytes(TurboAddress2, 2), TurboEnable);
+        set
+        {
+            if (value)
+            {
+                Memory.WriteBytes(TurboAddress1, TurboEnable);
+                Memory.WriteBytes(TurboAddress2, TurboEnable);
+            }
+            else
+            {
+                Memory.WriteBytes(TurboAddress1, TurboDisable1);
+                Memory.WriteBytes(TurboAddress2, TurboDisable2);
+            }
+        }
+    }
+
+    private static readonly byte[] TurboShadowEnable = [0x90, 0x90];
+    private static readonly byte[] TurboShadowDisable = [0x75, 0x57];
+    private readonly uint TurboShadowAddress;
+    /// <summary>
+    /// A property representing if <c>TurboShadow</c> is enabled in <see cref="Memory"/>.
+    /// </summary>
+    public bool TurboShadow
+    {
+        get => Enumerable.SequenceEqual(Memory.ReadBytes(TurboShadowAddress, 2), TurboShadowEnable);
+        set => Memory.WriteBytes(TurboShadowAddress, value ? TurboShadowEnable : TurboShadowDisable);
+    }
+
+    private static readonly byte[] CharacterShadowEnable = [0x90, 0x90];
+    private static readonly byte[] CharacterShadowDisable = [0x75, 0x09];
+    private readonly uint CharacterShadowAddress;
+    /// <summary>
+    /// A property representing if <c>CharacterShadow</c> is enabled in <see cref="Memory"/>.
+    /// </summary>
+    public bool CharacterShadow
+    {
+        get => Enumerable.SequenceEqual(Memory.ReadBytes(CharacterShadowAddress, 2), CharacterShadowEnable);
+        set => Memory.WriteBytes(CharacterShadowAddress, value ? CharacterShadowEnable : CharacterShadowDisable);
+    }
+
     internal Globals(Memory memory)
     {
         Memory = memory;
@@ -56,6 +117,15 @@ public sealed partial class Globals
         FeTextBible = new(Memory);
 
         LevelCountAddress = Memory.SelectAddress(0x4798A8, 0x479748, 0x479618, 0x4793D8) + 3;
+
         FlippableCarsAddress = Memory.SelectAddress(0x4E5020, 0x4E5100, 0x4E53C0, 0x4E51B0);
+
+        DefaultNumTurbosAddress = Memory.SelectAddress(0x4EF12A, 0x4EF23A, 0x4EF4DA, 0x4EF2DA) + 1;
+
+        TurboAddress1 = Memory.SelectAddress(0x4DB5B1, 0x4DB691, 0x4DB951, 0x4DB731);
+        TurboAddress2 = Memory.SelectAddress(0x4DB5B8, 0x4DB698, 0x4DB958, 0x4DB738);
+
+        TurboShadowAddress = Memory.SelectAddress(0x4E0D80, 0x4E0E60, 0x4E1120, 0x4E0F00);
+
     }
 }
