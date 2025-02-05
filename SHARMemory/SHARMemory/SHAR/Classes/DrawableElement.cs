fc3ee5854230729e81b57ef96d26deb7ec5c75ab
Proddy <josh@zhbot.org>
2025-02-05 17:07:29 +0000

    Implemented `DrawableElement`

--- SHARMemory/SHARMemory/SHAR/Classes/DrawableElement.cs
+++ SHARMemory/SHARMemory/SHAR/Classes/DrawableElement.cs
@@ -16,5 +16,43 @@ public class DrawableElement : Class
 
     public DrawableElement(Memory memory, uint address, CompleteObjectLocator completeObjectLocator) : base(memory, address, completeObjectLocator) { }
 
-    public Types Type => (Types)ReadInt32(4);
+    internal const uint VFTableOffset = 0;
+
+    internal const uint TypeOffset = VFTableOffset + sizeof(uint);
+    public Types Type
+    {
+        get => (Types)ReadUInt32(TypeOffset);
+        set => WriteUInt32(TypeOffset, (uint)value);
+    }
+
+    internal const uint VisibleOffset = TypeOffset + sizeof(uint);
+    public bool Visible
+    {
+        get => ReadBoolean(VisibleOffset);
+        set => WriteBoolean(VisibleOffset, value);
+    }
+
+    internal const uint LockVisibilityOffset = VisibleOffset + 1;
+    public bool LockVisibility
+    {
+        get => ReadBoolean(LockVisibilityOffset);
+        set => WriteBoolean(LockVisibilityOffset, value);
+    }
+
+    internal const uint IsTranslucentOffset = LockVisibilityOffset + 1;
+    public bool IsTranslucent
+    {
+        get => ReadBoolean(IsTranslucentOffset);
+        set => WriteBoolean(IsTranslucentOffset, value);
+    }
+
+    internal const uint SortOrderOffset = IsTranslucentOffset + 2; // Padding
+    public float SortOrder
+    {
+        get => ReadSingle(SortOrderOffset);
+        set => WriteSingle(SortOrderOffset, value);
+    }
+
+    internal const uint PoseOffset = SortOrderOffset + sizeof(float);
+    // public tPose Pose => Memory.ClassFactory.Create<Pose>(ReadUInt32(PoseOffset));
 }
