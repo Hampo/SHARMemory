52d515bf5e0e1cd34322a4053be992001b1e63e7
Proddy <josh@zhbot.org>
2025-07-17 13:18:22 +0100

    Fix CardsDB

--- SHARMemory/SHARMemory/SHAR/Classes/CardsDB.cs
+++ SHARMemory/SHARMemory/SHAR/Classes/CardsDB.cs
@@ -6,12 +6,14 @@ namespace SHARMemory.SHAR.Classes;
 [ClassFactory.TypeInfoName(".?AVCardsDB@@")]
 public class CardsDB : Class
 {
+    public const int MAX_NUM_CARDS = 64;
+
     public CardsDB(Memory memory, uint address, CompleteObjectLocator completeObjectLocator) : base(memory, address, completeObjectLocator) { }
 
     internal const uint CardDBVFTableOffset = 0;
 
     internal const uint CardsOffset = CardDBVFTableOffset + sizeof(uint);
-    public PointerArray<Card> Cards => new(Memory, ReadUInt32(CardsOffset), NumCards);
+    public PointerArray<Card> Cards => new(Memory, ReadUInt32(CardsOffset), MAX_NUM_CARDS);
 
     internal const uint NumCardsOffset = CardsOffset + sizeof(uint);
     public int NumCards
