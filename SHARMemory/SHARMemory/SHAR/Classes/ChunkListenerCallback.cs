77ccddff738c8dd098db734deb9bfeb0744951d2
Proddy <josh@zhbot.org>
2025-09-18 00:58:18 +0100

    feat/RenderManager populated

---
+++ SHARMemory/SHARMemory/SHAR/Classes/ChunkListenerCallback.cs
@@ -0,0 +1,10 @@
+﻿using SHARMemory.Memory;
+using SHARMemory.Memory.RTTI;
+
+namespace SHARMemory.SHAR.Classes;
+
+[ClassFactory.TypeInfoName(".?AVChunkListenerCallback@@")]
+public class ChunkListenerCallback : Class
+{
+    public ChunkListenerCallback(Memory memory, uint address, CompleteObjectLocator completeObjectLocator) : base(memory, address, completeObjectLocator) { }
+}
