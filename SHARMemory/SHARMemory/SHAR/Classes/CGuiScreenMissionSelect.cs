85ae7ad4dbbc81df343ae5452d2acc15317e64ef
Proddy <josh@zhbot.org>
2025-11-11 18:55:45 +0000

    feat/`CGuiScreenMissionSelect`

---
+++ SHARMemory/SHARMemory/SHAR/Classes/CGuiScreenMissionSelect.cs
@@ -0,0 +1,29 @@
+﻿using SHARMemory.Memory;
+using SHARMemory.Memory.RTTI;
+using SHARMemory.SHAR.Structs;
+
+namespace SHARMemory.SHAR.Classes;
+
+[ClassFactory.TypeInfoName(".?AVCGuiScreenMissionSelect@@")]
+public class CGuiScreenMissionSelect : CGuiScreen
+{
+    public const int MAX_NUM_REGULAR_MISSIONS = 7;
+
+    public CGuiScreenMissionSelect(Memory memory, uint address, CompleteObjectLocator completeObjectLocator) : base(memory, address, completeObjectLocator) { }
+
+    internal const uint NumLevelSelectionsOffset = PlayTransitionAnimationLastOffset + 4; // Padding
+    public int NumLevelSelections
+    {
+        get => ReadInt32(NumLevelSelectionsOffset);
+        set => WriteInt32(NumLevelSelectionsOffset, value);
+    }
+
+    internal const uint LeftArrowOffset = NumLevelSelectionsOffset + sizeof(int);
+    public FeEntity LeftArrow => Memory.ClassFactory.Create<FeEntity>(LeftArrowOffset);
+
+    internal const uint RightArrowOffset = LeftArrowOffset + sizeof(uint);
+    public FeEntity RightArrow => Memory.ClassFactory.Create<FeEntity>(RightArrowOffset);
+
+    internal const uint MissionInfoOffset = RightArrowOffset + sizeof(uint);
+    public StructArray<MissionDisplayInfo> MissionInfo => new(Memory, Address + MissionInfoOffset, MissionDisplayInfo.Size, MAX_NUM_REGULAR_MISSIONS);
+}
---
+++ SHARMemory/SHARMemory/SHAR/Structs/MissionDisplayInfo.cs
@@ -0,0 +1,53 @@
+﻿using SHARMemory.Memory;
+using SHARMemory.SHAR.Classes;
+using System;
+
+namespace SHARMemory.SHAR.Structs;
+
+[Struct(typeof(MissionDisplayInfoStruct))]
+public struct MissionDisplayInfo
+{
+    public const int Size = sizeof(uint) + sizeof(uint) + sizeof(uint);
+
+    public FeEntity Number;
+
+    public FeEntity Title;
+
+    public FeEntity Status;
+
+    public MissionDisplayInfo(FeEntity number, FeEntity title, FeEntity status)
+    {
+        Number = number;
+        Title = title;
+        Status = status;
+    }
+
+    public override readonly string ToString() => $"{Number} | {Title} | {Status}";
+}
+
+internal class MissionDisplayInfoStruct : Struct
+{
+    public override int Size => MissionDisplayInfo.Size;
+
+    public override object FromBytes(ProcessMemory Memory, byte[] Bytes, int Offset = 0)
+    {
+        FeEntity Number = Memory.ClassFactory.Create<FeEntity>(BitConverter.ToUInt32(Bytes, Offset));
+        Offset += sizeof(uint);
+        FeEntity Title = Memory.ClassFactory.Create<FeEntity>(BitConverter.ToUInt32(Bytes, Offset));
+        Offset += sizeof(uint);
+        FeEntity Status = Memory.ClassFactory.Create<FeEntity>(BitConverter.ToUInt32(Bytes, Offset));
+        return new MissionDisplayInfo(Number, Title, Status);
+    }
+
+    public override void ToBytes(ProcessMemory Memory, object Value, byte[] Buffer, int Offset = 0)
+    {
+        if (Value is not MissionDisplayInfo Value2)
+            throw new ArgumentException($"Argument '{nameof(Value)}' must be of type '{nameof(MissionDisplayInfo)}'.", nameof(Value));
+
+        BitConverter.GetBytes(Value2.Number?.Address ?? 0).CopyTo(Buffer, Offset);
+        Offset += sizeof(uint);
+        BitConverter.GetBytes(Value2.Title?.Address ?? 0).CopyTo(Buffer, Offset);
+        Offset += sizeof(uint);
+        BitConverter.GetBytes(Value2.Status?.Address ?? 0).CopyTo(Buffer, Offset);
+    }
+}
